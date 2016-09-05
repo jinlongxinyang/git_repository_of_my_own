d618b0191ec0f0d00828e871931f3fc4c8048298
jinlongxinyang <jinlongxinyang@126.com>
2016-09-05 21:22:58 +0800

    [+]add python os control scripy

---
+++ python_os_script.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#-*-coding: utf-8-*-
+
+# Version: 0.1
+# Author: Rebom <yrebom@gmail.com>
+# License: Copyright(c) 2015 Rebom
+# Summary: 
+import os
+import sys
+
+from datetime import datetime
